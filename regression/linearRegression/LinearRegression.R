b0665288660abae3637758061489927ea81ee007
Paul O'Hara <paul.j.o.hara@gmail.com>
2016-10-03 22:20:49 +0100

    Testing ssh

---
+++ regression/linearRegression/LinearRegression.R
@@ -0,0 +1,14 @@
+# Gradient Descent Algorihm
+
+# Loss Functions
+# sum squared error
+sumSquaredError <- function(theta, hypothesisCB) {
+  
+}
+
+# Linear regression 
+# this is h_theta(x)= = theta__0 + theta_1(x_1) + ... + theta_n(x_n)
+# setting x_0 is at 1
+linearRegression <-function(row, parameters) {
+  target <- 1 + sum(row * parameters)
+}
